e9f3fafeb8695c9e72abcd842017779348af98f4
spectrum <spectrick@gmail.com>
2015-09-16 19:48:22 +0900

    posts helper 추가

---
+++ client/posts.js
@@ -0,0 +1,20 @@
+Template.posts.helpers({
+   "posts": function() {
+       return [
+           {
+               author: {
+                   name: "Master",
+                   profile_image: "http://lorempixel.com/64/64/cats/"
+               },
+               message: "집사야 내 밥은 어딨냐?"
+           },
+           {
+               author: {
+                   name: "Slave4U",
+                   profile_image: "http://lorempixel.com/64/64/people/"
+               },
+               message: "배고파서 내가 먹었다."
+           }
+       ]
+   }
+});
