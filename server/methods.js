2aaeb1260dceb85cb4c27a81ef1fb5b214c3552a
spectrum <spectrick@gmail.com>
2015-09-18 11:25:29 +0900

    retro avatar 적용

--- server/methods.js
+++ server/methods.js
@@ -5,7 +5,7 @@ Meteor.methods({
             author: {
                 _id: this.userId,
                 name: Meteor.user().username,
-                profile_image: Gravatar.imageUrl(Meteor.user().emails[0].address)+"?d=retro"
+                profile_image: Gravatar.imageUrl(Meteor.user().emails[0].address, {d: "retro"})
             },
             pageId: obj.pageId,
             message: obj.message,
