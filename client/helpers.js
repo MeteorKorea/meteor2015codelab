9a73cccd2cd2f8e7686a41121c7566457ab7faa9
spectrum <spectrick@gmail.com>
2015-09-20 01:38:34 +0900

    reactive timer

--- client/helpers.js
+++
@@ -1,3 +0,0 @@
-Template.registerHelper('timeFrom', function(time) {
-    return moment().from(time);
-});
--- client/posts.js
+++ client/posts.js
@@ -3,14 +3,25 @@ Template.posts.onCreated(function() {
     this.autorun(function() {
         instance.subscribe('getPage', Session.get('pageId'));
     });
+    this.interval = Meteor.setInterval(function() {
+        Session.set('live', Random.id());
+    }, 1000);
 });
 
 Template.posts.helpers({
-   "posts": function() {
-       return Posts.find({}, {
-           sort: {
-               createdAt: -1
-           }
-       });
-   }
+    "posts": function () {
+        return Posts.find({}, {
+            sort: {
+                createdAt: -1
+            }
+        });
+    },
+    "timeFrom": function(time) {
+        Session.get('live');
+        return moment().from(time);
+    }
+});
+
+Template.posts.onDestroyed(function() {
+    Meteor.clearInterval(this.interval);
 });
