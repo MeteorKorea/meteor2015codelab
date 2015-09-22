cdc7d28912a7175505b7631054b084836a577514
spectrum <spectrick@gmail.com>
2015-09-23 05:57:17 +0900

    Feeling Lucky, RecentPosts publish

--- server/publish.js
+++ server/publish.js
@@ -1,3 +1,24 @@
 Meteor.publish('getPage', function(pageId) {
     return Posts.find({pageId: pageId});
 });
+
+Meteor.publish('recentPosts', function() {
+    var followings = this.userId &&
+        Meteor.users.findOne(this.userId).profile.followings || {};
+    var pageIds = [];
+    for (var id in followings ) {
+        pageIds.push(id);
+    }
+    return Posts.find({
+        pageId: {
+            $in: pageIds
+        }
+    })
+});
+
+Meteor.publish('feelingLucky', function() {
+    return Posts.find({}, {
+        skip: Math.random()*Posts.find().count(),
+        limit: 1
+    });
+});
