7268b50db95e512668e2d922f2e422509ee97e43
spectrum <spectrick@gmail.com>
2015-10-08 07:31:41 +0900

    Dashboard 수정. feeling lucky를 항상 활성화

--- client/dashboard.js
+++ client/dashboard.js
@@ -1,25 +1,16 @@
 Template.dashboard.onCreated(function() {
-    var instance=this;
-
-    this.autorun(function() {
-        var user = Meteor.user();
-        instance.handle && instance.handle.stop();
-        instance.handle = instance.subscribe(
-            user && 'recentPosts' || 'feelingLucky'
-        );
-    });
+    this.subscribe('feelingLucky');
 });
-
 Template.dashboard.helpers({
     'luckyPage': function() {
         var post = Posts.findOne()
         return post && post.pageId;
     },
     'pages': function() {
-        return _.unique(
-            _.pluck(Posts.find().fetch(), 'pageId')
-        ).map(function(v) {
-            return { pageId: v };
-        });
+        var result = [];
+        for (var i in Meteor.user().profile.followings) {
+          result.push({ pageId: i });
+        }
+        return result;
     }
 });
--- server/publish.js
+++ server/publish.js
@@ -2,20 +2,6 @@ Meteor.publish('getPage', function(pageId) {
     return Posts.find({pageId: pageId});
 });
 
-Meteor.publish('recentPosts', function() {
-    var followings = this.userId &&
-        Meteor.users.findOne(this.userId).profile.followings || {};
-    var pageIds = [];
-    for (var id in followings ) {
-        pageIds.push(id);
-    }
-    return Posts.find({
-        pageId: {
-            $in: pageIds
-        }
-    })
-});
-
 Meteor.publish('feelingLucky', function() {
     return Posts.find({}, {
         skip: Math.random()*Posts.find().count(),
